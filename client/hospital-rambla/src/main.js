3792416b8864fa65d71ad061308aac0369cb7692
alu0101333588 <alu0101333588@ull.edu.es>
2024-11-03 17:01:13 +0000

    Subida del frontend

---
+++ client/hospital-rambla/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  // Otras rutas pueden ir aquí
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
