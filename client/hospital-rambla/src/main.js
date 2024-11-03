84069fa48fdd845ab92355e7a59bed22c65912be
alu0101333588 <alu0101333588@ull.edu.es>
2024-11-03 17:21:38 +0000

    Arreglando fallos

--- client/hospital-rambla/src/main.js
+++ client/hospital-rambla/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 // import App from './App.vue'
 // import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router';
-import Inicio from '../views/Inicio.vue';
+import Inicio from '../components/Inicio.vue';
 
 
 Vue.use(VueRouter);
