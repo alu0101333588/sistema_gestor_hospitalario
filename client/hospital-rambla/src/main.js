0a88a7cc05e0883ef5d593769e702477041318f6
alu0101333588 <alu0101333588@ull.edu.es>
2024-11-03 18:03:04 +0000

    Arreglando errores

--- client/hospital-rambla/src/main.js
+++ client/hospital-rambla/src/main.js
@@ -1,28 +1,4 @@
-import Vue from 'vue'
-// import App from './App.vue'
-// import vuetify from './plugins/vuetify'
-import VueRouter from 'vue-router';
-import Inicio from '../components/Inicio.vue';
-import router from './router'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-
-createApp(App).use(router).use(vuetify).mount('#app')
-
-// Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'Inicio',
-    component: Inicio,
-  },
-  // Otras rutas pueden ir aquí
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
-
-export default router;
+createApp(App).mount('#app')
